fd2b1a97796afb8baefd8ada7c6c888078bd8f24
Ashley Marie Parker <ash.marie.parker@gmail.com>
2016-10-13 17:37:40 -0500

    add commands to run selection

---
+++ test/pyttbarfw/RunSemiLepTTbar_commands.sh
@@ -0,0 +1,46 @@
+#type 2 
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_ttjets_b2gtreeV3.root --outfile ttjets_outfile_type2.root --Type2 >& ttjets_outfile_type2_Test.txt &
+
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/b2gtree_SingleMuon_Run2016BCD.root --outfile data_BCD_outfile_type2.root --Type2 >& dataBCD_outfile_type2_Test.txt &
+
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets1_b2gtreeV3_incomplete.root --outfile wjets1_outfile_type2.root --Type2 >& wjets1_outfile_type2_Test.txt &
+
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets2_b2gtreeV3.root --outfile wjets2_outfile_type2.root --Type2 >& wjets2_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets3_b2gtreeV3.root --outfile wjets3_outfile_type2.root --Type2 >& wjets3_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets4_b2gtreeV3.root --outfile wjets4_outfile_type2.root --Type2 >& wjets4_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets5_b2gtreeV3.root --outfile wjets5_outfile_type2.root --Type2 >& wjets5_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets6_b2gtreeV3_incomplete.root --outfile wjets6_outfile_type2.root --Type2 >& wjets6_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets7_b2gtreeV3.root --outfile wjets7_outfile_type2.root --Type2 >& wjets7_outfile_type2_Test.txt &
+
+
+# type 1
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_ttjets_b2gtreeV3.root --outfile ttjets_outfile_type1.root  >& ttjets_outfile_type1_Test.txt &
+
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/b2gtree_SingleMuon_Run2016BCD.root --outfile data_BCD_outfile_type1.root  >& dataBCD_outfile_type1_Test.txt &
+
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets1_b2gtreeV3_incomplete.root --outfile wjets1_outfile_type1.root  >& wjets1_outfile_type1_Test.txt &
+
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets2_b2gtreeV3.root --outfile wjets2_outfile_type1.root  >& wjets2_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets3_b2gtreeV3.root --outfile wjets3_outfile_type1.root  >& wjets3_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets4_b2gtreeV3.root --outfile wjets4_outfile_type1.root >& wjets4_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets5_b2gtreeV3.root --outfile wjets5_outfile_type1.root  >& wjets5_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets6_b2gtreeV3_incomplete.root --outfile wjets6_outfile_type1.root  >& wjets6_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets7_b2gtreeV3.root --outfile wjets7_outfile_type1.root  >& wjets7_outfile_type1_Test.txt &
