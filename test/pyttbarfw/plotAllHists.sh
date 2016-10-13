15a84ca40d8220f3cb2dd6354de9b216c87b8efe
Ashley Marie Parker <ash.marie.parker@gmail.com>
2016-10-13 17:42:11 -0500

    list of all possible histos to plot

---
+++ test/pyttbarfw/plotAllHists.sh
@@ -0,0 +1,23 @@
+python -i plotstack.py --hist AK8MSDHist 
+
+python -i plotstack.py --hist AK8PtHist
+
+python -i plotstack.py --hist AK8EtaHist
+
+python -i plotstack.py --hist AK8Tau21Hist
+
+python -i plotstack.py --hist AK8Tau32Hist
+
+python -i plotstack.py --hist AK8MHist
+
+python -i plotstack.py --hist AK8MSDSJ0Hist
+
+python -i plotstack.py --hist LeptonPtHist
+
+python -i plotstack.py --hist LeptonEtaHist
+
+python -i plotstack.py --hist METPtHist
+
+python -i plotstack.py --hist HTLepHist
+
+python -i plotstack.py --hist Iso2DHist  
