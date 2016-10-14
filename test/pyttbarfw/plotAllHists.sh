35109f66e6b15234a27fcb595cde3b7ebccf8885
Ashley Marie Parker <ash.marie.parker@gmail.com>
2016-10-14 07:52:56 -0500

    more plot commands

--- test/pyttbarfw/plotAllHists.sh
+++ test/pyttbarfw/plotAllHists.sh
@@ -1,23 +1,51 @@
-python -i plotstack.py --hist AK8MSDHist 
+python plotstack.py --hist AK8MSDHist 
 
-python -i plotstack.py --hist AK8PtHist
+python plotstack.py --hist AK8PtHist 
 
-python -i plotstack.py --hist AK8EtaHist
+python plotstack.py --hist AK8EtaHist
 
-python -i plotstack.py --hist AK8Tau21Hist
+python plotstack.py --hist AK8Tau21Hist 
 
-python -i plotstack.py --hist AK8Tau32Hist
+python plotstack.py --hist AK8Tau32Hist 
 
-python -i plotstack.py --hist AK8MHist
+python plotstack.py --hist AK8MHist
 
-python -i plotstack.py --hist AK8MSDSJ0Hist
+python plotstack.py --hist AK8MSDSJ0Hist
 
-python -i plotstack.py --hist LeptonPtHist
+python plotstack.py --hist LeptonPtHist 
 
-python -i plotstack.py --hist LeptonEtaHist
+python plotstack.py --hist LeptonEtaHist
 
-python -i plotstack.py --hist METPtHist
+python plotstack.py --hist METPtHist
+ 
+python plotstack.py --hist HTLepHist 
 
-python -i plotstack.py --hist HTLepHist
+python plotstack.py --hist Iso2DHist 
 
-python -i plotstack.py --hist Iso2DHist  
+python plotstack.py --hist AK4BdiscHist
+
+python plotstack.py --hist AK8MSDSJ0Pt200To300Hist
+python plotstack.py --hist AK8MSDSJ0Pt300To400Hist
+python plotstack.py --hist AK8MSDSJ0Pt400To500Hist
+python plotstack.py --hist AK8MSDSJ0Pt500To600Hist
+
+'''
+TO-DO: add explanation of options ( --Type2)
+TO-DO : Add commands for the following after implementing them in plotstack.py
+       
+            self.AK8MPt200To300Hist
+            self.AK8MSDPt200To300Hist
+
+            self.AK8MPt300To400Hist
+            self.AK8MSDPt300To400Hist
+
+            self.AK8MPt400To500Hist
+            self.AK8MSDPt400To500Hist
+            self.AK8MPt500To600Hist
+            self.AK8MSDPt500To600Hist
+
+            self.AK8MPt600To800Hist
+            self.AK8MSDPt600To800Hist
+            self.AK8MSDSJ0Pt600To800Hist
+
+'''
