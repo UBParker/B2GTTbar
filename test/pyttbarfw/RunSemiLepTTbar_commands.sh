788b38990b70b180f22c3144c0d5d079cb0edae0
Ashley Marie Parker <ash.marie.parker@gmail.com>
2016-10-16 22:28:26 -0500

    adding commands for ST samples

--- test/pyttbarfw/RunSemiLepTTbar_commands.sh
+++ test/pyttbarfw/RunSemiLepTTbar_commands.sh
@@ -2,12 +2,9 @@
 
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_ttjets_b2gtreeV3.root --outfile ttjets_outfile_type2.root --Type2 >& ttjets_outfile_type2_Test.txt &
 
-
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/b2gtree_SingleMuon_Run2016BCD.root --outfile data_BCD_outfile_type2.root --Type2 >& dataBCD_outfile_type2_Test.txt &
 
-
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets1_b2gtreeV3_incomplete.root --outfile wjets1_outfile_type2.root --Type2  --ignoreTrig >& wjets1_outfile_type2_Test.txt &
-
 
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets2_b2gtreeV3.root --outfile wjets2_outfile_type2.root --Type2  --ignoreTrig >& wjets2_outfile_type2_Test.txt &
 
@@ -21,17 +18,24 @@ python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B
 
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets7_b2gtreeV3.root --outfile wjets7_outfile_type2.root --Type2  --ignoreTrig >& wjets7_outfile_type2_Test.txt &
 
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st1_b2gtreeV3_incomplete.root --outfile st1_outfile_type2.root --Type2  --ignoreTrig >& st1_incomplete_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st2_b2gtreeV3.root --outfile st2_outfile_type2.root --Type2  --ignoreTrig >& st2_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st3_b2gtreeV3.root --outfile st3_outfile_type2.root --Type2  --ignoreTrig >& st3_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st4_b2gtreeV3.root --outfile st4_outfile_type2.root --Type2  --ignoreTrig >& st4_outfile_type2_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st5_b2gtreeV3.root --outfile st5_outfile_type2.root --Type2  --ignoreTrig >& st5_outfile_type2_Test.txt &
+
 
 # type 1
 
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_ttjets_b2gtreeV3.root --outfile ttjets_outfile_type1.root   >& ttjets_outfile_type1_Test.txt &
 
-
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/b2gtree_SingleMuon_Run2016BCD.root --outfile data_BCD_outfile_type1.root  >& dataBCD_outfile_type1_Test.txt &
 
-
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets1_b2gtreeV3_incomplete.root --outfile wjets1_outfile_type1.root  --ignoreTrig  >& wjets1_outfile_type1_Test.txt &
-
 
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets2_b2gtreeV3.root --outfile wjets2_outfile_type1.root  --ignoreTrig  >& wjets2_outfile_type1_Test.txt &
 
@@ -44,3 +48,13 @@ python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets6_b2gtreeV3_incomplete.root --outfile wjets6_outfile_type1.root  --ignoreTrig  >& wjets6_outfile_type1_Test.txt &
 
 python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_wjets7_b2gtreeV3.root --outfile wjets7_outfile_type1.root  --ignoreTrig  >& wjets7_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st1_b2gtreeV3_incomplete.root --outfile st1_outfile_type1.root  --ignoreTrig >& st1_incomplete_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st2_b2gtreeV3.root --outfile st2_outfile_type1.root  --ignoreTrig >& st2_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st3_b2gtreeV3.root --outfile st3_outfile_type1.root  --ignoreTrig >& st3_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st4_b2gtreeV3.root --outfile st4_outfile_type1.root --ignoreTrig >& st4_outfile_type1_Test.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_st5_b2gtreeV3.root --outfile st5_outfile_type1.root  --ignoreTrig >& st5_outfile_type1_Test.txt &
