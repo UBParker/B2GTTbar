f26a9ba127679760bda91985e4707d310671df52
Ashley Marie Parker <ash.marie.parker@gmail.com>
2017-01-10 13:47:44 -0600

    dict of histogram titles

---
+++ test/pyttbarfw/hist/titles.py
@@ -0,0 +1,48 @@
+'''
+# TO-DO : Add all possible histo names to HistoTitles
+
+
+title = HistoTitles[self.histoName][0]
+rangeMin = HistoTitles[self.histoName][1]
+rangeMax = HistoTitles[self.histoName][2]
+'''
+
+HistoTitles =            {     "AK8MSDHist" :  [ "AK8 Jet SD Mass (GeV)" ,  0.0  ,   300. ],  
+                              "AK8MSDSJ0Hist" : [ "Leading Subjet SD Mass (GeV)" , 39.0  ,   150. ],             
+                              "AK8PtHist" : [ "AK8 Jet P_{T} (GeV)", 0.     ,  1000. ],
+                              "AK8EtaHist" : [   "AK8 Jet #eta", -2.5   ,    2.5 ],
+                              "AK8puppitau21Hist" : [ "AK8 puppi Jet #tau_{21}", 0.0    ,    1.0 ],
+                              "AK8puppitau32Hist" : [ "AK8 puppi Jet #tau_{32}", 0.0    ,    1.0 ],
+                              "AK8MHist" : [ "AK8 Jet Mass (GeV)", 0.0    ,   300. ],
+                              "LeptonPtHist" : [ "Lepton P_{T} (GeV)", 0.0    ,   600. ],
+                              "LeptonEtaHist" : [ "Lepton #eta", 2.5    ,  -2.5  ],
+                              "METPtHist" : [ "Missing P_{T} (GeV)", 0.    ,   1000. ],
+                              "HTLepHist" : [  "Lepton P_{T} + Missing P_{T} (GeV)", 0.     ,   700. ],
+                              #"Iso2DHist" : [ "Lepton 2D isolation (#Delta R vs p_{T}^{REL} )", 0.     ,   300. ],
+                              "AK4BdiscHist" : [ "CSVv2 B Disc", 0. , 1. ],
+                              "AK8MSDSJ0Pt200To300Hist" : [ "(200<P_{t}<300) Subjet 0 SD Mass (GeV)", 45.    ,   151. ], # TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MSDSJ0Pt300To400Hist" : [  "(300<P_{t}<400) Subjet 0 SD Mass (GeV)", 45.    ,   151. ], # TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MSDSJ0Pt400To500Hist" : [  "(400<P_{t}<500) Subjet 0 SD Mass (GeV)", 45.    ,   151. ], # TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MSDSJ0Pt500To800Hist" : [  "(500<P_{t}<800) Subjet 0 SD Mass (GeV)", 45.    ,   151. ], # TO-DO : [ move pt label elsewhere on canvas
+                              "AK8HTHist" : [ "AK8 Jet H_{T} (GeV)", 0.     ,  1000. ],
+                              "AK8SDPtHist" : [  "AK8 SD Jet P_{T} (GeV)", 0.     ,  1000. ],
+                              "AK8PuppiSDPtHist" : [ "AK8 PUPPI SD Jet P_{T} (GeV)", 0.     ,  1000. ],
+                              "AK8PuppiPtHist" : [  "AK8 PUPPI SD Jet P_{T} (GeV)", 0.     ,  1000. ],
+                              "AK8MPt200To300Hist" : [ "(200<P_{t}<300)  AK8 Jet Mass (GeV)", 0.    ,   300.   ], # TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MPt300To400Hist" : [ "(300<P_{t}<400)  AK8 Jet Mass (GeV)", 0.    ,   300.   ], # TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MPt400To500Hist" : [ "(400<P_{t}<500)  AK8 Jet Mass (GeV)", 0.    ,   300.   ],# TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MPt500To800Hist" : [ "(500<P_{t}<800)  AK8 Jet Mass (GeV)", 0.    ,   300.   ],# TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MSDPt200To300Hist" : [ "(200<P_{t}<300)  AK8 SD Jet Mass (GeV)", 0.    ,   300.   ],# TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MSDPt300To400Hist" : [  "(300<P_{t}<400)  AK8 SD Jet Mass (GeV)", 0.    ,   300.   ],# TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MSDPt400To500Hist" : [ "(400<P_{t}<500)  AK8 SD Jet Mass (GeV)", 0.    ,   300.   ],# TO-DO : [ move pt label elsewhere on canvas
+                              "AK8MSDPt500To800Hist" : [ "(500<P_{t}<800)  AK8 SD Jet Mass (GeV)", 0.    ,   300.   ],# TO-DO : [ move pt label elsewhere on canvas
+                              "AK8Tau32Hist" : [ "AK8 Jet #tau_{32}", 0.0    ,    1.0 ]
+                              }
+
+                                 
+                                 
+                                 
+                                
+                                 
+                                 
+                                                
