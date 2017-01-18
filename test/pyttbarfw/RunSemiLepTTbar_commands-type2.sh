31ad31e6a1944f3be26f3a3a674eb16db0b162e5
Ashley Marie Parker <Om@Ashleys-MacBook-Pro.local>
2017-01-17 21:11:29 -0600

    new commands

--- test/pyttbarfw/RunSemiLepTTbar_commands-type2.sh
+++ test/pyttbarfw/RunSemiLepTTbar_commands-type2.sh
@@ -1,43 +1,58 @@
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016B.root --Type2 --outfile singlemu_run2016B_hists_Type2.root  >& singlemu_run2016B_hists_Type2.txt &
 
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_ttbar_all_V4.root --Type2 --outfile ttbar_outfileType2.root >& ttbar_outfileType2_out.log &
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016C.root --Type2 --outfile singlemu_run2016C_hists_Type2.root  >& singlemu_run2016C_hists_Type2.txt &
 
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_100_V4.root --Type2 --outfile wjets100to200_outfileType2.root >& wjets100to200_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_200_V4.root --Type2 --outfile wjets200to400_outfileType2.root >& wjets200to400_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_400_V4.root --Type2 --outfile wjets400to600_outfileType2.root >& wjets400to600_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_600_V4.root --Type2 --outfile wjets600to800_outfileType2.root >& wjets600to800_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_800_V4.root  --Type2 --outfile wjets800to1200_outfileType2.root >& wjets800to1000_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_1200_V4.root --Type2 --outfile wjets1200to2500_outfileType2.root >& wjets1200to2500_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_2500_V4.root --Type2 --outfile wjets2500toinf_outfileType2.root >& wjets2500toinf_outfileType2_out.log &
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016D.root --Type2 --outfile singlemu_run2016D_hists_Type2.root  >& singlemu_run2016D_hists_Type2.txt &
 
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016E.root --Type2 --outfile singlemu_run2016E_hists_Type2.root  >& singlemu_run2016E_hists_Type2.txt &
 
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_data_Run2016B_all_V4.root --Type2 --outfile run2016b_outfileType2.root  >& run2016b_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_data_Run2016C_all_V4.root --Type2 --outfile run2016c_outfileType2.root  >& run2016c_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_data_Run2016D_all_V4.root --Type2 --outfile run2016d_outfileType2.root  >& run2016d_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_mudata_Run2016B-23Sep_all_V4.root --Type2 --outfile run2016b_mudata_outfileType2.root >& run2016b_mudata_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_mudata_Run2016F-23Sep_all_V4.root --Type2 --outfile run2016f_mudata_outfileType2.root >& run2016f_mudata_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_mudata_Run2016G-23Sep_all_V4.root --Type2 --outfile run2016g_mudata_outfileType2.root >& run2016g_mudata_outfileType2_out.log &
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/aparker/B2G2016/V4Trees/b2gtree_singlemu_Run2016F-23Sep_all_V4.root --Type2 --outfile singlemu_run2016F_hists_Type2.root  >& singlemu_run2016F_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/aparker/B2G2016/V4Trees/b2gtree_singlemu_Run2016G-23Sep_all_V4.root --Type2 --outfile singlemu_run2016G_hists_Type2.root >& singlemu_run2016G_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016H.root --Type2 --outfile singlemu_run2016H_hists_Type2.root  >& singlemu_run2016H_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/haddFiles/Puppi_ttjets_b2gtreeV3.root --Type2 --outfile ttjets__hists_Type2.root    >& ttjets_hists_Type2.txt &
 
 
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_100_V4.root --Type2 --outfile wjets_100to200_hists_Type2.root --ignoreTrig >& wjets100_hists_Type2.txt &
 
-python RunSemiLepTTbar.py --infile root://cmsxrootd.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016B.root --Type2 --outfile singlemu_run2016B_histosType2.root >& singlemu_run2016B_histosType2.txt &
-python RunSemiLepTTbar.py --infile root://cmsxrootd.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016C.root --Type2 --outfile singlemu_run2016C_histosType2.root >& singlemu_run2016C_histosType2.txt &
-python RunSemiLepTTbar.py --infile root://cmsxrootd.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016D.root --Type2 --outfile singlemu_run2016D_histosType2.root >& singlemu_run2016D_histosType2.txt &
-python RunSemiLepTTbar.py --infile root://cmsxrootd.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016E.root --Type2 --outfile singlemu_run2016E_histosType2.root >& singlemu_run2016E_histosType2.txt &
-python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/aparker/B2G2016/V4Trees/b2gtree_mudata_Run2016F-23Sep_all_V4.root --Type2 --outfile singlemu_run2016F_histosType2.root >& singlemu_run2016F_histosType2.txt &
-python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/aparker/B2G2016/V4Trees/b2gtree_mudata_Run2016G-23Sep_all_V4.root --Type2 --outfile singlemu_run2016G_histosType2.root >& singlemu_run2016G_histosType2.txt &
-python RunSemiLepTTbar.py --infile root://cmsxrootd.fnal.gov//store/user/rappocc/B2GAnaFWFiles16Dec2016/singlemu_run2016H.root --Type2 --outfile singlemu_run2016H_histosType2.root >& singlemu_run2016H_histosType2.txt &
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_200_V4.root --Type2 --outfile wjets_200to400_hists_Type2.root --ignoreTrig >& wjets200_hists_Type2.txt &
 
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht100_V4.root --Type2 --outfile qcd100_outfileType2.root >& qcd100_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht200_V4.root --Type2 --outfile qcd200_outfileType2.root >& qcd200_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht300_V4.root --Type2 --outfile qcd300_outfileType2.root >& qcd300_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht500_V4.root --Type2 --outfile qcd500_outfileType2.root >& qcd500_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht700_V4.root --Type2 --outfile qcd700_outfileType2.root >& qcd700_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht1000_V4.root --Type2 --outfile qcd1000_outfileType2.root >& qcd1000_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht1500_V4.root --Type2 --outfile qcd1500_outfileType2.root >& qcd1500_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht2000_V4.root --Type2 --outfile qcd2000_outfileType2.root >& qcd2000_outfileType2_out.log &
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_400_V4.root --Type2 --outfile wjets_400to600_hists_Type2.root --ignoreTrig >& wjets400_hists_Type2.txt &
 
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_ST_schannel_V4.root --Type2 --outfile singletop_schannel_outfileType2.root >& singletop_schannel_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_ST_tW-top_V4.root --Type2 --outfile singletop_tW_outfileType2.root >& singletop_tW_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_ST_tchannel-antitop_V4.root --Type2 --outfile singletop_tchannel_outfileType2.root >& singletop_tchannel_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile root://131.225.207.127:1094//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_ST_tW-antitop_V4.root --Type2 --outfile singletop_tWantitop_outfileType2.root >& singletop_tWantitop_outfileType2_out.log &
-python RunSemiLepTTbar.py --infile /uscms_data/d2/rappocc/analysis/B2G/CMSSW_8_0_22/src/Analysis/B2GTTbar/test/pyttbarfw/b2gtree_MC_ST_tchannel-top_V4.root --Type2 --outfile singletop_tchanneltop_outfileType2.root >& singletop_tchanneltop_outfileType2_out.log &
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_600_V4.root --Type2 --outfile wjets_600to800_hists_Type2.root --ignoreTrig >& wjets600_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_800_V4.root --Type2 --outfile wjets_800to1200_hists_Type2.root --ignoreTrig >& wjets800_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_1200_V4.root --Type2 --outfile wjets_1200to2500_hists_Type2.root --ignoreTrig >& wjets1200_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_wjets_2500_V4.root --Type2 --outfile wjets_2500toInf_hists_Type2.root --ignoreTrig >& wjets2500_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_ST_schannel_V4.root --Type2 --outfile ST_schannel_hists_Type2.root --ignoreTrig >& ST_schannel_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_ST_tW-top_V4.root --Type2 --outfile ST_tW-top_hists_Type2.root --ignoreTrig >& ST_tW-top_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_ST_tchannel-antitop_V4.root --Type2 --outfile ST_tchannel-antitop_hists_Type2.root --ignoreTrig >& ST_tchannel-antitop_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtreeV4_ST_t-channel_top_1of1.root --Type2 --outfile ST_t-channel_top_hists_Type2.root --ignoreTrig >& ST_t-channel_top_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtreeV4_ST_tW_antitop_1of1.root --Type2 --outfile ST_tW_antitop_hists_Type2.root --ignoreTrig >& ST_tW_antitop_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht100_V4.root --Type2 --outfile QCD_Ht100_hists_Type2.root --ignoreTrig >& QCD_Ht100_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht200_V4.root --Type2 --outfile QCD_Ht200_hists_Type2.root --ignoreTrig >& QCD_Ht200_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht300_V4.root --Type2 --outfile QCD_Ht300_hists_Type2.root --ignoreTrig >& QCD_Ht300_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht500_V4.root --Type2 --outfile QCD_Ht500_hists_Type2.root --ignoreTrig >& QCD_Ht500_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht700_V4.root --Type2 --outfile QCD_Ht700_hists_Type2.root --ignoreTrig >& QCD_Ht700_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht1000_V4.root --Type2 --outfile QCD_Ht1000_hists_Type2.root --ignoreTrig >& QCD_Ht1000_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht1500_V4.root --Type2 --outfile QCD_Ht1500_hists_Type2.root --ignoreTrig >& QCD_Ht1500_hists_Type2.txt &
+
+python RunSemiLepTTbar.py --infile root://cmseos.fnal.gov//store/user/asparker/B2G2016/V4Trees/b2gtree_MC_QCD_Ht2000_V4.root --Type2 --outfile QCD_Ht2000_hists_Type2.root --ignoreTrig >& QCD_Ht2000_hists_Type2.txt &
+
+
