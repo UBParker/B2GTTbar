f482debae917a5e926a7a5c2f0f885396b5bca3c
UBParker <ash.marie.parker@gmail.com>
2017-05-09 00:54:26 -0500

    preliminary dR btw W cand subjet and gen matched quarks

--- test/pyttbarfw/genstudies_fwlite.py
+++ test/pyttbarfw/genstudies_fwlite.py
@@ -1,6 +1,6 @@
->#! /usr/bin/env python
+#! /usr/bin/env python
 
-# python unittest_cmstt.py
+# Adapted from the example script  unittest_cmstt.py
 
 import ROOT
 import sys
@@ -36,8 +36,8 @@ files = [  #"root://cmseos.fnal.gov///store/user/jdolen/B2G2016/ZprimeToTT_M-300
         ]
 events = Events (files)
 
-#h_ak8Jets =  Handle ("std::vector<pat::Jet>")           SD + PUPPI AK8 info not stored but 4-vector is  equal to sum of 2 subjets
-#l_ak8Jets =         ("selectedPatJetsAK8PFPuppi")
+h_ak8Jets =  Handle ("std::vector<pat::Jet>")        #   SD + PUPPI AK8 info not stored but 4-vector is  equal to sum of 2 subjets
+l_ak8Jets =  ("slimmedJetsAK8")      # ("selectedPatJetsAK8PFPuppi")
 
 h_ak8SubJets =  Handle ("std::vector<pat::Jet>")
 l_ak8SubJets =         ("slimmedJetsAK8PFPuppiSoftDropPacked", "SubJets" )
@@ -61,6 +61,7 @@ ievent = 0
 for event in events:
   if options.maxevents > 0 :
     if (options.maxevents == ievent ): break
+    event.getByLabel (l_ak8Jets, h_ak8Jets)
     event.getByLabel (l_ak8SubJets, h_ak8SubJets)
     event.getByLabel (l_ak4Jets, h_ak4Jets)
     event.getByLabel (l_MET, h_MET)
@@ -68,7 +69,7 @@ for event in events:
     event.getByLabel (l_Muon, h_Muon)
     event.getByLabel (l_GenParticle, h_GenParticle)
     #print"AK8 jets product is {}".format(h_ak8Jets.product())
-    #ak8Jets = h_ak8Jets.product()
+    ak8JetsCHS  = h_ak8Jets.product()
     ak8SubJets = h_ak8SubJets.product()
     ak4Jets = h_ak4Jets.product()
     met = h_MET.product()
@@ -155,7 +156,7 @@ for event in events:
         ### Get the Ws which decay - record their daughter information
         ### W+
         elif PDGid==24 :
-          if options.verbose: print"....W+ with 2 daughters  id {} statusIs {} ndau {} pt {} eta {} phi {}".format(id, statusIs, nDau, pt, eta, phi)
+          if options.verbose: print"....W+ with 2 daughters  id {0} statusIs {1} ndau {2} pt {3:2.2f} eta {4:2.2f} phi {5:2.2f}".format(PDGid, statusIs, nDau, pt, eta, phi)
           if options.verbose: print"......W+ dd0 ID {} ndau {} ".format(particle.daughter( 0 ).pdgId(), particle.daughter( 0 ).numberOfDaughters())
           if options.verbose: print"......W+ dd1 ID {} ndau {} ".format(particle.daughter( 1 ).pdgId(), particle.daughter( 1 ).numberOfDaughters())
           Wtd1_p4.SetPxPyPzE( particle.daughter( 0 ).px(), particle.daughter( 0 ).py(), particle.daughter( 0 ).pz(), particle.daughter( 0 ).energy() )
@@ -166,7 +167,7 @@ for event in events:
           Wtd2_id = particle.daughter( 1 ).pdgId()
         ### W-
         elif PDGid==-24 :
-          if options.verbose: print"....W- with 2 daughters  id {} statusIs {} ndau {} pt {} eta {} phi {}".format(id, statusIs, nDau, pt, eta, phi)
+          if options.verbose: print"....W- with 2 daughters  id {0} statusIs {1} ndau {2} pt {3:2.2f} eta {4:2.2f} phi {5:2.2f}".format(PDGid, statusIs, nDau, pt, eta, phi)
           if options.verbose: print"......W- dd0 ID {} ndau {} ".format(particle.daughter( 0 ).pdgId(), particle.daughter( 0 ).numberOfDaughters())
           if options.verbose: print"......W- dd1 ID {} ndau {} ".format(particle.daughter( 1 ).pdgId(), particle.daughter( 1 ).numberOfDaughters())
           Watd1_p4.SetPxPyPzE( particle.daughter( 0 ).px(), particle.daughter( 0 ).py(), particle.daughter( 0 ).pz(), particle.daughter( 0 ).energy() )
@@ -181,23 +182,93 @@ for event in events:
     if (tophadronic  and not antitophadronic)  : GenTruth_semileptonic = True
     if ( not tophadronic and antitophadronic)  : GenTruth_semileptonic = True
 
-    ### For the semi-leptonic decays find the reco level top , W and b candidates
-    if GenTruth_semileptonic :
-      for isj , sj in enumerate(ak8SubJets):
-        print" got isj {}, isj.pt() {}  ".format(sj, sj.pt())
-        ### Find leading and sublead subjets and store their 4-vectors
-        ### Higher mass subjet is W candidate
+    ### Save 4-vectos of all subjets
+    sj_p4 = [] 
 
-        ### Add 2 subjet P4s to get AK8 4-vector
+    ### Find the 2 subjets which are closest to the AK8, where sjA is the first 2 subjets and B is the second 2 if they exist then close and far refer to dR from AK8 axis                                                 
+    sjAcloseak8_p4 = None
+    sjAfarak8_p4 = None
+    sjBcloseak8_p4 = None
+    sjBfarak8_p4 = None
+    
+    ### The 2 subjets from above that were closer to the AK8 jet axis
+    sjcloseak8_p4 = None
+    sjfarak8_p4 = None
+
+    ### For the semi-leptonic decays (including hadronic too for now) find the reco level top , W and b candidates
+    if GenTruth_semileptonic or GenTruth_hadronic:
+      ### Find the leading AK8 CHS jet
+      chsjet_p4 = None
+      for ijet, chsjet in enumerate(ak8JetsCHS):
+        ### Only consider leading AK8
+        #if ijet >= 1 : break
+        ### Ensure a boosted topology 
+        if chsjet.pt() < 400. : continue          
+        chsjet_p4= ROOT.TLorentzVector()
+        chsjet_p4.SetPtEtaPhiM(chsjet.pt(), chsjet.eta(), chsjet.phi(), chsjet.mass() )
+      ### Find the 4-vectos of all  PUPPI  subjets  
+      if chsjet_p4 != None:
+        print"........Reco AK8 jet of Pt {0:2.2f} > 400 GeV".format(chsjet_p4.Pt())
+        tempSJ_p4 = ROOT.TLorentzVector()
+        for isj , sj in enumerate(ak8SubJets):
+          tempSJ_p4.SetPtEtaPhiM(sj.pt(), sj.eta(), sj.phi(), sj.mass() )
+          dR_sj_ak8 = tempSJ_p4.DeltaR(chsjet_p4)
+          print"..........Puppi SoftDrop subjet number {0} of pt {1:2.2f} dR(subjet, AK8) {2:2.2f} ".format(isj, sj.pt(), dR_sj_ak8)
+          sj_p4.append(tempSJ_p4)
+        ### Determine which of the subjets are closest to the AK8 axis 
+        dRMin_ak8_sj0  = 1000.
+        dRMin2_ak8_sj0 = 1000.
+        dRMin_ak8_sj1  = 1000.
+        dRMin2_ak8_sj1 = 1000.
+        for isjet, sjet in enumerate(sj_p4):
+          if isjet <= 1:
+            if chsjet_p4.DeltaR(sjet) < dRMin_ak8_sj0  : 
+              dRMin_ak8_sj0 = chsjet_p4.DeltaR(sjet)
+              sjAcloseak8_p4 = sjet
+            for issjet, sjet in enumerate(sj_p4):
+              if issjet == isjet : continue
+              if ( dRMin_ak8_sj0 < chsjet_p4.DeltaR(sjet) < dRMin2_ak8_sj0)  :
+                dRMin2_ak8_sj0 = chsjet_p4.DeltaR(sjet)
+                sjAfarak8_p4 = sjet  
+          if isjet > 1:
+            if chsjet_p4.DeltaR(sjet) < dRMin_ak8_sj1  :
+              dRMin_ak8_sj1 = chsjet_p4.DeltaR(sjet)
+              sjBcloseak8_p4 = sjet
+            for issjet, sjet in enumerate(sj_p4):
+              if issjet== isjet : continue
+              if ( dRMin_ak8_sj1 < chsjet_p4.DeltaR(sjet) < dRMin2_ak8_sj1)  :
+                dRMin2_ak8_sj0 = chsjet_p4.DeltaR(sjet)
+                sjBfarak8_p4 = sjet
+        if sjBcloseak8_p4 != None:
+          if dRMin_ak8_sj1 < dRMin_ak8_sj0 :
+            sjcloseak8_p4 = sjBcloseak8_p4
+            sjfarak8_p4 = sjBfarak8_p4
+          if dRMin_ak8_sj1 >  dRMin_ak8_sj0 :
+            sjcloseak8_p4 = sjAcloseak8_p4
+            sjfarak8_p4 = sjAfarak8_p4
+        else:
+          sjcloseak8_p4 = sjAcloseak8_p4
+          sjfarak8_p4 = sjAfarak8_p4
+    if sjcloseak8_p4  != None and sjfarak8_p4  != None:      
+      ### Higher mass subjet is W candidate
+      if sj0_p4.M() > sj1_p4.M() :
+        WCand_p4.SetPtEtaPhiM(sj0_p4.Pt(), sj0_p4.Eta(), sj0_p4.Phi(), sj0_p4.M() )
+        bCand_p4.SetPtEtaPhiM(sj1_p4.Pt(), sj1_p4.Eta(), sj1_p4.Phi(), sj1_p4.M() )
+      else :
+        bCand_p4.SetPtEtaPhiM(sj0_p4.Pt(), sj0_p4.Eta(), sj0_p4.Phi(), sj0_p4.M() )
+        WCand_p4.SetPtEtaPhiM(sj1_p4.Pt(), sj1_p4.Eta(), sj1_p4.Phi(), sj1_p4.M() )
+      print"......W candidate PUPPI + SD subjet of pt {0:2.2f} and mass {1:2.2f}".format(WCand_p4.Perp() , WCand_p4.M())
+      print"......b quark candidate PUPPI + SD subjet of pt {0:2.2f} and mass {1:2.2f}".format(WCand_p4.Perp() , WCand_p4.M())
+
+      hadtopCand_p4 = bCand_p4 + WCand_p4      ### Add 2 subjet P4s to get AK8 4-vector
 
         #if iak8.Pt() < 400. : continue ### To ensure a boosted topology
 
-        ### Save the 4-vector of Reco level objects                                                             
+        #### We now have gen particles and reco subjets so we need DeltaRs btw them and then plot those dRs in 2D histos
 
-        #hadtopCand_p4 = SetPtEtaPhiM( iak8.Pt(), iak8.Eta(), iak8.Phi() , iak8.M()  )
-        #bCand_p4 = ROOT.TLorentzVector()
-        # WCand_p4 = ROOT.TLorentzVector() ### Most massive SD subjet of hadronic top candidate AK8 jet                 
-
+if  WCand_p4 != None and Wtd1_p4 != None :       
+  dR_WCand_Wtd1 = WCand_p4.DeltaR(Wtd1_p4 )
+  print"dR btw W candidate subjet of pt {} from ak8 of pt {} and W daugter quark 1 of pt {} is {}".format(WCand_p4.Pt(), hadtopCand_p4.Pt(), Wtd1_p4.Pt(), dR_WCand_Wtd1 )
 
 ievent +=1
 ### End Event Loop
